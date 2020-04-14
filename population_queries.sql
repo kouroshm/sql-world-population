a4b5a338f1bbca8b9869f989a61501a5764cd87c
Kourosh <kourosh.motavalizadeh@gmail.com>
2020-04-14 15:17:57 -0400

    Add files via upload

---
+++ population_queries.sql
@@ -0,0 +1,52 @@
+-- How many entries in the database are from Africa?
+SELECT count(*)
+FROM countries
+WHERE continent = 'Africa';
+
+
+-- What was the total population of Oceania in 2005?
+SELECT SUM(population)
+FROM population_years
+INNER JOIN countries
+ON countries.id = population_years.country_id 
+WHERE continent = 'Oceania' AND year = 2005; 
+
+
+-- What is the average population of countries in South America in 2003?
+SELECT AVG(population)
+FROM population_years
+INNER JOIN countries
+ON countries.id = population_years.country_id
+WHERE year = 2003 AND continent = 'South America';
+
+
+-- What country had the smallest population in 2007?
+SELECT name,MIN(population)
+FROM population_years
+INNER JOIN countries
+ON countries.id = population_years.country_id
+WHERE year = 2007;
+
+
+-- What is the average population of Poland during the time period covered by this dataset?
+SELECT name, AVG(population) as 'Average Population'
+FROM population_years
+INNER JOIN countries
+ON countries.id = population_years.country_id
+WHERE name = 'Poland';
+
+
+-- How many countries have the word "The" in their name?
+SELECT count(*)
+FROM countries
+WHERE name like '%The%';
+
+
+-- What was the total population of each continent in 2010?
+SELECT  SUM(population) as 'Population in 2010', continent 
+FROM population_years
+INNER JOIN countries
+ON countries.id = population_years.country_id
+WHERE year = 2010
+GROUP BY 2;
+
