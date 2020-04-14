18ade32f1b31fc841cc5e86bb4c2bd69d921a498
Kourosh <kourosh.motavalizadeh@gmail.com>
2020-04-14 15:25:12 -0400

    Update population_queries.sql

--- population_queries.sql
+++ population_queries.sql
@@ -1,11 +1,11 @@
 -- How many entries in the database are from Africa?
-SELECT count(*)
+SELECT count(*) as 'Entries'
 FROM countries
 WHERE continent = 'Africa';
 
 
 -- What was the total population of Oceania in 2005?
-SELECT SUM(population)
+SELECT SUM(population) as 'Total Population'
 FROM population_years
 INNER JOIN countries
 ON countries.id = population_years.country_id 
@@ -13,7 +13,7 @@ WHERE continent = 'Oceania' AND year = 2005;
 
 
 -- What is the average population of countries in South America in 2003?
-SELECT AVG(population)
+SELECT AVG(population) as 'Average Population'
 FROM population_years
 INNER JOIN countries
 ON countries.id = population_years.country_id
@@ -21,7 +21,7 @@ WHERE year = 2003 AND continent = 'South America';
 
 
 -- What country had the smallest population in 2007?
-SELECT name,MIN(population)
+SELECT name, MIN(population) as 'Smallest Population'
 FROM population_years
 INNER JOIN countries
 ON countries.id = population_years.country_id
@@ -37,7 +37,7 @@ WHERE name = 'Poland';
 
 
 -- How many countries have the word "The" in their name?
-SELECT count(*)
+SELECT count(*) as 'Number of Countries with "The"'
 FROM countries
 WHERE name like '%The%';
 
